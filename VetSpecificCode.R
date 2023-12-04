14c4cfb9c69d2ac072a0860217cb5efc0f01c3e2
Breanna Niekamp <bre.niekamp@gmail.com>
2023-12-03 18:42:38 -0700

    updated stuff in file

--- VetSpecificCode.R
+++ VetSpecificCode.R
@@ -3,11 +3,3 @@ mcesdat = read.csv("mces_data20231122.csv")
 destdat = read.csv("destination_data20231122.csv")
 surveydat = read.csv("survey_data20231122.csv")
 solutionfunddat = read.csv("solutions_fund20231122.csv")
-
-# Exploring housing destinations and rapidity of housing allocation for veterans. 
-
-# Investigating the duration veterans spend in MCES compared to non-veterans. Has this changed over time?
-
-
-# Analyze the most common prior living situations for veterans
-
